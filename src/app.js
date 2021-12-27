a3afdf043e3fc29bd6d64e5df9eae9a2db2e4a52
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-12-27 23:48:46 +0100

    init: project configuration

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+
+const App = () => <h1>Hello, world!</h1>;
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: `./src/app.js`,
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.min.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: `./src/index.html`,
+            filename: 'index.html',
+        }),
+        new ESLintPlugin(),
+    ],
+};
