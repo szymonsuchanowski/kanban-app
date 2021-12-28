9aef6daf48c7ea89bcf502fa364cc1d8fb62b5cd
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-12-28 23:02:11 +0100

    feat: add columns & init tasks data

---
+++ src/helpers/columnsData.js
@@ -0,0 +1,9 @@
+const columnsData = [
+    { id: 1, name: 'pending', limit: 6, isDivided: false },
+    { id: 2, name: 'analysis', limit: 3, isDivided: true },
+    { id: 3, name: 'development', limit: 5, isDivided: true },
+    { id: 4, name: 'test', limit: 3, isDivided: false },
+    { id: 5, name: 'deploy', limit: 5, isDivided: false },
+];
+
+export default columnsData;
